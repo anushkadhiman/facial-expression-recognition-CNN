d58b26a057bd2b95ebb0da738f137e3b28136abc
anushkadhiman <54269874+anushkadhiman@users.noreply.github.com>
2019-11-09 18:02:26 +0530

    Add files via upload

---
+++ faceimgtest.py
@@ -0,0 +1,42 @@
+from __future__ import division
+#from keras.models import Sequential
+from sklearn.externals import joblib
+#from keras.layers import Dense
+import numpy
+#import os
+import numpy as np
+import cv2
+
+#loading the model
+model=joblib.load("C:/Users/AD/Downloads/expression-face.pkl") 
+print("Loaded model from disk")
+
+#setting image resizing parameters
+WIDTH = 48
+HEIGHT = 48
+x=None
+y=None
+labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+#loading image
+full_size_image = cv2.imread("C:/Users/AD/Downloads/4disguested.jpg")
+print("Image Loaded")
+gray=cv2.cvtColor(full_size_image,cv2.COLOR_RGB2GRAY)
+face_cascade=cv2.CascadeClassifier('C:/Users/AD/Documents/My documents/Projects/Machine Learning/face recognisation/haarcascade_frontalface_default.xml')
+faces = face_cascade.detectMultiScale(gray, 1.3  , 10)
+
+#detecting faces
+for (x, y, w, h) in faces:
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
+        cv2.rectangle(full_size_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        #predicting the emotion
+        yhat= model.predict(cropped_img)
+        cv2.putText(full_size_image, labels[int(np.argmax(yhat))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+        print("Emotion: "+labels[int(np.argmax(yhat))])
+        path = 'C:/Users/AD/Downloads/imgprd.png' #path to save the image
+        cv2.imwrite(path,full_size_image)  #to save the image
+
+cv2.imshow('Emotion', full_size_image)
+cv2.waitKey()
